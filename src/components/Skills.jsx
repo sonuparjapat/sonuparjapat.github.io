74106a827ddb35ee99d44301d34b2ac3b823fd76
sonuparjapat <sahil992001parjapat@gmail.com>
2023-03-30 13:36:20 +0530

    changesadded

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,11 +1,34 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Heading,Image } from '@chakra-ui/react'
 import React from 'react'
-
+import styles from "./Skill.module.css"
+import { useDisclosure,useColorMode } from '@chakra-ui/react';
 export default function Skills() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { colorMode, toggleColorMode } = useColorMode();
+  const isDark = colorMode === "dark";
   return (
-    <Box  height={"600px"} bg="orange" id="skills">
-<h3>Skills</h3>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas cumque vel nemo. Assumenda, id a.</p>
+    <Box bg={isDark&&"gray.800"} height={"600px"}  id="skills">
+<Heading mt="20px" as="h6" textAlign={"center"} >SKILLS</Heading>
+<Box margin="auto" mt="50px" height="400px" className={styles.box3} width="80%" >
+<Box className="skills-card-name" ml={['2em', '3em', '4em', '4em', '4em', '4em']} width="98%" height="100%"  paddingTop="60px"    display={"grid"}  gridTemplateColumns={{base:"repeat(3,1fr)",md:"repeat(3,1fr)",lg:"repeat(3,1fr)",xl:"repeat(4,1fr)"}}>
+<Box className="skills-card" width={['2em', '3em', '4em', '4em', '4em', '4em']} ><Image className="skills-card-img" id={styles.box4} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"/></Box>
+<Box width={['2em', '3em', '4em', '4em', '4em', '4em']} ><Image className={styles.box4} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"/></Box>
+<Box className="skills-card" width={['2em', '3em', '4em', '4em', '4em', '4em']} ><Image className="skills-card-img"  id={styles.box4} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"/></Box>
+<Box className="skills-card" width={['2em', '3em', '4em', '4em', '4em', '4em']} ><Image className="skills-card-img" id={styles.box4} src="https://cdn-icons-png.flaticon.com/128/3291/3291667.png"/></Box>
+<Box className="skills-card" width={['2em', '3em', '4em', '4em', '4em', '4em']} ><Image className="skills-card-img"  id={styles.box4} src="https://cdn-icons-png.flaticon.com/128/2906/2906274.png"/></Box>
+<Box className="skills-card" width={['2em', '3em', '4em', '4em', '4em', '4em']} ><Image className="skills-card-img" id={styles.box4} src="https://cdn-icons-png.flaticon.com/512/4248/4248443.png"/></Box>
+<Box className="skills-card" width={['2em', '3em', '4em', '4em', '4em', '4em']} ><Image className="skills-card-img" id={styles.box4}  src="https://cdn-icons-png.flaticon.com/512/919/919825.png"/></Box>
+<Box className="skills-card" width={['2em', '3em', '4em', '4em', '4em', '4em']} ><Image className="skills-card-img" id={styles.box4} src="https://images.opencollective.com/chakra-ui/eb43264/logo/256.png"/></Box>
+
+
+
+</Box>
+
+</Box>
+
+
+
+
     </Box>
   )
 }
